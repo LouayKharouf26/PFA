1a0b8969932856dd4c55f522ae01e1477b4c17a2
Malek-Zaag <zaag.malek1@gmail.com>
2023-02-24 22:31:27 +0100

    first commit

--- src/controller/controller.js
+++ src/controller/controller.js
@@ -4,3 +4,6 @@ module.exports.getDefault = function (req, res) {
     console.log("hello for get request");
     res.send("hello for test request");
 };
+module.exports.addUser = function (req, res) {
+    var body = req.body;
+};
--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -4,3 +4,7 @@ module.exports.getDefault = (req: express.Request, res: express.Response) => {
   console.log("hello for get request");
   res.send("hello for test request");
 };
+
+module.exports.addUser = (req: express.Request, res: express.Response) => {
+  const body = req.body;
+};
