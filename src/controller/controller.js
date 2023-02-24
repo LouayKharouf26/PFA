a6394c715c754828af0e5e809e27167bb67d0f00
Malek-Zaag <zaag.malek1@gmail.com>
2023-02-24 23:43:46 +0100

    .gitignore is now working

--- src/controller/controller.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-module.exports.getDefault = function (req, res) {
-    console.log("hello for get request");
-    res.send("hello for test request");
-};
-module.exports.addUser = function (req, res) {
-    var body = req.body;
-};
--- src/router/router.ts
+++
@@ -1,7 +0,0 @@
-import * as express from "express";
-
-const router = express.Router();
-const controller = require("../controller/controller");
-router.get("/", controller.getDefault);
-
-module.exports = router;
