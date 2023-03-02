57e8aa4bac3bf36a650838f72540fd6c4824ea74
Malek-Zaag <zaag.malek1@gmail.com>
2023-03-03 00:13:25 +0100

    linux template done

--- terraform template/linux.tf
+++ terraform template/linux.tf
@@ -9,9 +9,109 @@ terraform {
 }
 
 provider "azurerm" {
-  features {
-
-  }
+  features {}
   subscription_id = var.subscription_id
 }
 
+resource "azurerm_resource_group" "resource-group" {
+  name     = var.resource_group_name
+  location = "West Europe"
+}
+
+resource "azurerm_network_security_group" "network-security-group" {
+  name                = "default-security-group"
+  location            = azurerm_resource_group.resource-group.location
+  resource_group_name = azurerm_resource_group.resource-group.name
+  security_rule = [{
+    name                       = "ssh-in"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+    },
+    {
+      name                       = "ping"
+      priority                   = 100
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Icmp"
+      source_port_range          = "*"
+      destination_port_range     = "*"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    },
+
+    {
+      name                       = "connexion"
+      priority                   = 100
+      direction                  = "Outbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = "22"
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+  }]
+}
+
+resource "azurerm_virtual_network" "virtual-network" {
+  name                = var.virtual_network_name
+  location            = azurerm_resource_group.resource-group.location
+  resource_group_name = azurerm_resource_group.resource-group.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.resource-group.name
+  virtual_network_name = azurerm_virtual_network.virtual-network.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet_network_security_group_association" "name" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.network-security-group.id
+}
+
+resource "azurerm_network_interface" "network-interface" {
+  name                = "${var.virtual_machine_name}-nic"
+  location            = azurerm_resource_group.resource-group.location
+  resource_group_name = azurerm_resource_group.resource-group.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+
+resource "azurerm_linux_virtual_machine" "linux-virtual-machine" {
+  name                = var.virtual_machine_name
+  resource_group_name = azurerm_resource_group.resource-group.name
+  location            = azurerm_resource_group.resource-group.location
+  size                = var.virtual_machine_size
+  admin_username      = "admin"
+  network_interface_ids = [
+    azurerm_network_interface.network-interface.id,
+  ]
+  #   admin_ssh_key {
+  #     username   = "adminuser"
+  #     public_key = file("~/.ssh/id_rsa.pub")
+  #   }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    # user defined image
+    # publisher = "Canonical"
+    # offer     = "UbuntuServer"
+    # sku       = "16.04-LTS"
+    # version   = "latest"
+  }
+
+}
--- terraform template/variables.tf
+++ terraform template/variables.tf
@@ -1,1 +1,18 @@
-variable "subscription_id" {}
+variable "subscription_id" {
+
+}
+variable "resource_group_name" {
+
+}
+variable "virtual_network_name" {
+
+}
+variable "subnet_name" {
+
+}
+variable "virtual_machine_name" {
+
+}
+variable "virtual_machine_size" {
+
+}
