171cbd93ef77506430195e863b9c66a200e52a30
Malek-Zaag <zaag.malek1@gmail.com>
2023-04-04 00:23:44 +0100

    .

--- terraform-template/windows_server/script.ps1
+++ terraform-template/windows_server/script.ps1
@@ -1,10 +1,6 @@
 Start-Process powershell -Verb RunAs
-$url = "https://www.python.org/ftp/python/3.10.2/python-3.10.2-amd64.exe"
-$output = "C:\Python\python-3.10.2-amd64.exe"
-
-Invoke-WebRequest -Uri $url -OutFile $output
-$pythonInstaller = "C:\Python\python-3.10.2-amd64.msi"
-$installPath = "C:\Python\Python310"
-$installArgs = "/qn ADDLOCAL=ALL"
-
-Start-Process -FilePath msiexec -ArgumentList "/i $pythonInstaller TARGETDIR=$installPath $installArgs" -Wait
+$script = New-Object Net.WebClient
+$script.DownloadString("https://chocolatey.org/install.ps1")
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+choco upgrade chocolatey
+choco install -y python3
