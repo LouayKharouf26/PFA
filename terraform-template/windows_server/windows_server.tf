e7889a4cd8e3024a95612b0501528d9aed2703aa
Malek-Zaag <zaag.malek1@gmail.com>
2023-04-06 00:50:35 +0100

    .

--- terraform-template/windows_server/windows_server.tf
+++ terraform-template/windows_server/windows_server.tf
@@ -160,11 +160,11 @@ resource "azurerm_windows_virtual_machine" "windows-server-virtual-machine" {
 }
 
 resource "azurerm_virtual_machine_extension" "web_server_install" {
-  name                       = "${var.virtual_machine_name}-wsi"
+  name                       = "${var.virtual_machine_name}3-wsi"
   virtual_machine_id         = azurerm_windows_virtual_machine.windows-server-virtual-machine.id
   publisher                  = "Microsoft.Compute"
   type                       = "CustomScriptExtension"
-  type_handler_version       = "1.8"
+  type_handler_version       = "2.0"
   auto_upgrade_minor_version = true
 
   settings = <<SETTINGS
