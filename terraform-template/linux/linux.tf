02fe70ff19c3d40b9bee7d991b58cc795598a54d
Malek-Zaag <zaag.malek1@gmail.com>
2023-05-27 13:04:20 +0100

    .

--- terraform-template/linux/linux.tf
+++ terraform-template/linux/linux.tf
@@ -1,9 +1,8 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      #version = "3.54.0"
-      version = "~>3.0.0"
+      source  = "hashicorp/azurerm"
+      version = "3.54.0"
     }
   }
   required_version = ">= 0.14.9"
