d6c1bfe72d3713b20f11463216b98e58b4af5d16
Malek-Zaag <zaag.malek1@gmail.com>
2023-05-28 11:45:25 +0100

    .

--- terraform-template/windows/windows.tf
+++ terraform-template/windows/windows.tf
@@ -2,11 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0.0"
+      version = "3.54.0"
     }
   }
   required_version = ">= 0.14.9"
 }
+
 
 provider "azurerm" {
   features {}
